2a6f6ad5e1fec54f067cbb1bcda130eca106c1fe
hab278 <hab278@nyu.edu>
2017-03-17 14:47:09 -0400

    Sample testem tests

---
+++ js/src/pds_login.js
@@ -0,0 +1,32 @@
+/*
+ * Handle passive login to PDS
+ *
+ * Ex.
+ *  pdsLogin.passiveLogin();
+ */
+const pdsLogin = {
+  location: () => {
+    return window.location;
+  },
+  hostname: () => {
+    return pdsLogin.location().hostname;
+  },
+  pdsUrl: () => {
+    const institute = 'NYU';
+    const url = '.library.nyu.edu/pds?func=load-login&calling_system=aleph&institute=' + institute + '&url=' + encodeURIComponent(pdsLogin.location());
+    if (pdsLogin.hostname().match("^alephstage")) return 'https://pdsdev' + url;
+    if (pdsLogin.hostname().match("^aleph")) return 'https://pds' + url;
+    return pdsLogin.hostname();
+  },
+  isLoggedIn: () => {
+    return cookies.get('_aleph_pds_passive_login');
+  },
+  redirectToPds: () => {
+    cookies.set('_aleph_pds_passive_login');
+    location.replace(pdsLogin.pdsUrl());
+  },
+  passiveLogin: () => {
+    if (pdsLogin.isLoggedIn()) return;
+    pdsLogin.redirectToPds();
+  }
+};
