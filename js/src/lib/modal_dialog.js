ce645622f560a35cca9d971c42fc69d755ad2e61
Eric Griffis <eric.griffis@nyu.edu>
2017-06-06 15:39:56 -0400

    Complete modal dialog move; needs continued refactor

--- js/src/lib/modal_dialog.js
+++ js/src/lib/modal_dialog.js
@@ -1,4 +1,3 @@
-//INCOMPLETE
 const modalDialog = {
   pdsLoginRegex: /LoginPds/,
   pdsUrlRegex: /var url = '([^\?]*\?func=load-login\&calling_system=aleph\&institute=[^\&]*\&url=)/,
@@ -9,34 +8,35 @@ const modalDialog = {
     return this.pdsUrlRegex.exec(data)[1] + encodeURIComponent(currentUrl);
   },
   launchDialog(data, sharedModalD) {
+    var $sharedModalD = $(sharedModalD);
     var feedbackText = $.trim($(data).children("div#feedbackbar p").text());
     var main = $(data).children("div#content div#main").get(0);
     var heading = $(main).find("h3").eq(0).remove();
     heading.find("span.subdue").remove();
-    sharedModalD.html(main);
+    $sharedModalD.html(main);
     //Add title
     var title = jQuery("#holdings table#bib td.fxxx").first().text();
     $(sharedModalD).find("div#main").eq(0).prepend(jQuery("<h3>"+title+"</h3>"));
     //Add feedback
     if (feedbackText.length > 0) {
-      var feedback = $("<div class=\"feedback\">"+feedback_text+"</div>");
+      var feedback = $("<div class=\"feedback\">"+feedbackText+"</div>");
       //shared_modal_d.prepend(feedback);
-      sharedModalD.html(feedback);
+      $sharedModalD.html(feedback);
     }
-    var is_request_ill = jQuery.data(shared_modal_d, "is_request_ill");
+    var is_request_ill = $sharedModalD.data("is_request_ill");
     if(is_request_ill) {
-      var doc_number = jQuery.data(shared_modal_d, "doc_number");
-      var doc_library = jQuery.data(shared_modal_d, "doc_library");
-      jQuery(shared_modal_d).find("div#main form ol#request_options").eq(0).append(jQuery(bs_ill_item(doc_number, doc_library)));
+      var doc_number = $sharedModalD.data("doc_number");
+      var doc_library = $sharedModalD.data("doc_library");
+      $(sharedModalD).find("div#main form ol#request_options").eq(0).append(jQuery(bs_ill_item(doc_number, doc_library)));
     }
-    var is_available = jQuery.data(shared_modal_d, "is_available");
-    var is_offsite = jQuery.data(shared_modal_d, "is_offsite");
+    var is_available = $sharedModalD.data("is_available");
+    var is_offsite = $sharedModalD.data("is_offsite");
     if(is_available || is_offsite) {
-        var sub_library = jQuery.data(shared_modal_d, "sub_library");
-      jQuery(shared_modal_d).find("div#main span#sub_library").eq(0).replaceWith(sub_library);
+      var sub_library = $sharedModalD.data("sub_library");
+      $sharedModalD.find("div#main span#sub_library").eq(0).replaceWith(sub_library);
     }
-          jQuery(shared_modal_d).dialog("option", "title", heading.text());
-    jQuery(shared_modal_d).dialog("open");
+    $sharedModalD.dialog("option", "title", heading.text());
+    $sharedModalD.dialog("open");
   },
   launchDialogOrRedirect(sharedModalDialog, targetUrl, currentUrl, inputData) {
     $.get(targetUrl, inputData, function(data, textStatus, xmlHttpRequest) {
@@ -45,7 +45,7 @@ const modalDialog = {
         // Grab the pds url and redirect
         location.replace(this.pdsLoginUrl(data, currentUrl));
       } else {
-
+        this.launchDialog(data, sharedModalD);
       }
     });
   }
