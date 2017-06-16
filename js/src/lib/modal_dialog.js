24e3c266877e85075be45e6f99a8537d6e6d5a62
Eric Griffis <eric.griffis@nyu.edu>
2017-06-16 16:34:00 -0400

    Refactor shared modal to attribute

--- js/src/lib/modal_dialog.js
+++ js/src/lib/modal_dialog.js
@@ -33,38 +33,37 @@ const modalDialog = {
   	return html;
   },
   launchDialog(data, sharedModalD) {
-    var $sharedModalD = $(sharedModalD);
     var feedbackText = $.trim($(data).children("div#feedbackbar p").text());
     var main = $(data).children("div#content div#main").get(0);
     var heading = $(main).find("h3").eq(0).remove();
     heading.find("span.subdue").remove();
-    $sharedModalD.html(main);
+    this.$sharedModalDialog.html(main);
     //Add title
     var $title = $("<h3>" + this.getTitleText() + "</h3>");
-    $sharedModalD.find("div#main").eq(0).prepend($title);
+    this.$sharedModalDialog.find("div#main").eq(0).prepend($title);
     //Add feedback
     if (feedbackText.length > 0) {
       var feedback = $("<div class=\"feedback\">"+feedbackText+"</div>");
       //shared_modal_d.prepend(feedback);
-      $sharedModalD.html(feedback);
+      $sharedModalDialog.html(feedback);
     }
-    var is_request_ill = $sharedModalD.data("is_request_ill");
+    var is_request_ill = this.$sharedModalDialog.data("is_request_ill");
     if(is_request_ill) {
-      var doc_number = $sharedModalD.data("doc_number");
-      var doc_library = $sharedModalD.data("doc_library");
+      var doc_number = this.$sharedModalDialog.data("doc_number");
+      var doc_library = this.$sharedModalDialog.data("doc_library");
       var $illItem = $(this.getIllItem(doc_number, doc_library));
       $(sharedModalD).find("div#main form ol#request_options").eq(0).append($illItem);
     }
-    var is_available = $sharedModalD.data("is_available");
-    var is_offsite = $sharedModalD.data("is_offsite");
+    var is_available = this.$sharedModalDialog.data("is_available");
+    var is_offsite = this.$sharedModalDialog.data("is_offsite");
     if(is_available || is_offsite) {
-      var sub_library = $sharedModalD.data("sub_library");
-      $sharedModalD.find("div#main span#sub_library").eq(0).replaceWith(sub_library);
+      var sub_library = this.$sharedModalDialog.data("sub_library");
+      this.$sharedModalDialog.find("div#main span#sub_library").eq(0).replaceWith(sub_library);
     }
-    $sharedModalD.dialog("option", "title", heading.text());
-    $sharedModalD.dialog("open");
+    this.$sharedModalDialog.dialog("option", "title", heading.text());
+    this.$sharedModalDialog.dialog("open");
   },
-  launchDialogOrRedirect(sharedModalDialog, targetUrl, currentUrl, inputData) {
+  launchDialogOrRedirect(targetUrl, currentUrl, inputData) {
     var obj = this;
     $.get(targetUrl, inputData, function(data, textStatus, xmlHttpRequest) {
       // Check to see if we're logging into PDS.
@@ -72,42 +71,42 @@ const modalDialog = {
         // Grab the pds url and redirect
         location.replace(obj.pdsLoginUrl(data, currentUrl));
       } else {
-        obj.launchDialog(data, sharedModalDialog);
+        obj.launchDialog(data);
       }
     });
   },
   init() {
     var obj = this;
-    var shared_modal_d = this.getSharedModalDialog();
+    this.$sharedModalDialog = this.getSharedModalDialog();
     $("#holdings table#items td.links a").filter(function(){
       return $(this).text().match(/^Request$/);
     }).addClass("ajax_window");
   	$("#holdings table#items td.links a.ajax_window").live("click", function(event) {
   		var target_url = this.href;
   		var current_url = window.location;
-      var holding = new Holding(this, shared_modal_d);
+      var holding = new Holding(this, obj.$sharedModalDialog);
       holding.extractAndSetData();
-  		obj.launchDialogOrRedirect(shared_modal_d, target_url, current_url);
+  		obj.launchDialogOrRedirect(target_url, current_url);
   		return false;
   	});
   	$("form.modal_dialog_form input[type=submit]").live("click", function(event) {
   		var target_url = jQuery(event.target).closest("form").attr("action");
   		var input_data = jQuery(event.target).closest("form").serialize();
   		var current_url = window.location;
-      var holding = new Holding(this, shared_modal_d);
+      var holding = new Holding(this, obj.$sharedModalDialog);
       holding.extractAndSetData();
   		jQuery(shared_modal_d).dialog({close: function(event, ui) {location.reload();}});
-  		obj.launchDialogOrRedirect(shared_modal_d, target_url, current_url, input_data);
+  		obj.launchDialogOrRedirect(target_url, current_url, input_data);
   		return false;
   	});
   	$("form.modal_dialog_form").live( "submit", function(event) {
   		var target_url = jQuery(event.target).closest("form").attr("action");
   		var input_data = jQuery(event.target).closest("form").serialize();
   		var current_url = window.location;
-      var holding = new Holding(this, shared_modal_d);
+      var holding = new Holding(this, obj.$sharedModalDialog);
       holding.extractAndSetData();
   		$(shared_modal_d).dialog({close: function(event, ui) {location.reload();}});
-  		obj.launchDialogOrRedirect(shared_modal_d, target_url, current_url, input_data);
+  		obj.launchDialogOrRedirect(target_url, current_url, input_data);
   		return false;
   	});
   }
