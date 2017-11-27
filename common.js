28eba4936f4ce7e8454e0684945a0cf24a29d1de
Barnaby Alter <barnaby.alter@nyu.edu>
2017-11-27 12:40:00 -0500

    Fix file common.js is pointing to even if its just a reference file

--- common.js
+++ common.js
@@ -3,8 +3,8 @@ document.domain = 'library.nyu.edu';
 //Load Aleph initialize JavaScript if not already loaded.
 if (typeof(window.AlephInit) == "undefined") {
   if (window.location.hostname.match("^alephstage")) {
-    document.write('<script type="text/javascript" src="https://cdn-dev.library.nyu.edu/aleph/application.js"><\/script>');
+    document.write('<script type="text/javascript" src="https://cdn-dev.library.nyu.edu/aleph/application.min.js"><\/script>');
   } else {
-    document.write('<script type="text/javascript" src="https://cdn.library.nyu.edu/aleph/application.js"><\/script>');
+    document.write('<script type="text/javascript" src="https://cdn.library.nyu.edu/aleph/application.min.js"><\/script>');
   }
 }
